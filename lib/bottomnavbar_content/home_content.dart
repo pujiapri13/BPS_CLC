30d0b69ecab28725b0522fa710be5cde2bd46797
pujiapri13 <40346046+pujiapri13@users.noreply.github.com>
2022-08-22 10:55:16 +0700

    Update home_content.dart

--- lib/bottomnavbar_content/home_content.dart
+++ lib/bottomnavbar_content/home_content.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: library_private_types_in_public_api
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeContent(),
+    ));
 
 class HomeContent extends StatefulWidget {
   const HomeContent({Key? key}) : super(key: key);
@@ -13,8 +15,144 @@ class HomeContent extends StatefulWidget {
 class _HomeContentState extends State<HomeContent> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Konten Home'),
-    );
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(12),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          //Button 1
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: Colors.white),
+              child: const Text(
+                "Inflasi",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+
+          // Button 2
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: Colors.white),
+              child: const Text(
+                "Ketimpangan",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+
+          //Button 3
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: Colors.white),
+              child: const Text(
+                "Pengangguran",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+
+          //Button 4
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: Colors.white),
+              child: const Text(
+                "Pertumbuhan Ekonomi",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          //Button 5
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: Colors.white),
+              child: const Text(
+                "Tenaga Kerja",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          //Button 6
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: Colors.white),
+              child: const Text(
+                "Kemiskinan",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          //Button 7
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: Colors.white),
+              child: const Text(
+                "Jumlah Penduduk",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          //akhir button
+        ],
+      ),
+    ));
   }
 }
