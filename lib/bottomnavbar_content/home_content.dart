490bc5510d360257ea106b86f986416c692d5f60
pujiapri13 <apriyantopuji13@gmail.com>
2022-08-24 18:05:30 +0700

    unmerger

--- lib/bottomnavbar_content/home_content.dart
+++ lib/bottomnavbar_content/home_content.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: library_private_types_in_public_api
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -20,8 +18,124 @@ class _HomeContentState extends State<HomeContent> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Konten Home'),
+    const screenHeight = 633.5;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          //animasi atas
+          SizedBox(
+            height: screenHeight * 0.24,
+            width: double.infinity,
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                CarouselSlider(
+                  items: carouselList.map((e) {
+                    return Builder(
+                      builder: (context) {
+                        return AnimatedContainer(
+                          duration: const Duration(milliseconds: 1000),
+                          curve: Curves.easeOutBack,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 6, 107, 133),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
+                  options: CarouselOptions(
+                    height: screenHeight * 0.24,
+                    viewportFraction: 0.98,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    reverse: false,
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          Row(
+            children: <Widget>[
+              //satu
+              Container(
+                width: screenWidth * 0.55,
+                height: screenHeight * 0.19,
+                color: Colors.redAccent,
+                child: const Center(
+                  child: Text('INFLASI'),
+                ),
+              ),
+              //dua
+              Container(
+                width: screenWidth * 0.45,
+                height: screenHeight * 0.19,
+                color: Colors.red,
+                child: const Center(
+                  child: Text('KETIMPANGAN'),
+                ),
+              ),
+            ],
+          ),
+
+          Row(
+            children: <Widget>[
+              //tiga
+              Container(
+                width: screenWidth * 0.45,
+                height: screenHeight * 0.19,
+                color: Colors.red,
+                child: const Center(
+                  child: Text('PENGANGGURAN'),
+                ),
+              ),
+              //empat
+              Container(
+                width: screenWidth * 0.55,
+                height: screenHeight * 0.19,
+                color: Colors.redAccent,
+                child: const Center(
+                  child: Text('PERTUMBUHAN EKONOMI'),
+                ),
+              ),
+            ],
+          ),
+
+          Row(
+            children: <Widget>[
+              //lima
+              Container(
+                width: screenWidth * 0.55,
+                height: screenHeight * 0.19,
+                color: Colors.redAccent,
+                child: const Center(
+                  child: Text('TENAGA KERJA'),
+                ),
+              ),
+              //enam
+              Container(
+                width: screenWidth * 0.45,
+                height: screenHeight * 0.19,
+                color: Colors.red,
+                child: const Center(
+                  child: Text('KEMISKINAN'),
+                ),
+              ),
+            ],
+          ),
+          //tujuh
+          Container(
+            width: screenWidth,
+            height: screenHeight * 0.19,
+            color: const Color.fromARGB(255, 114, 19, 12),
+            child: const Center(
+              child: Text('JUMLAH PENDUDUK'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
