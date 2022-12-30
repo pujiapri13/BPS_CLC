cbb9edc8fcc0f8c527649ac6314e91eb46e197a1
pujiapri13 <apriyantopuji13@gmail.com>
2022-12-30 18:53:14 +0700

    .

--- lib/pdrb/pdrb_adhk_mlu.dart
+++ lib/pdrb/pdrb_adhk_mlu.dart
@@ -29,6 +29,140 @@ class _PdrbAdhkLapUsState extends State<PdrbAdhkLapUs> {
             style: TextStyle(fontSize: 16),
           ),
           backgroundColor: Colors.black,
+          actions: [
+            IconButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  backgroundColor: Colors.white,
+                  constraints: BoxConstraints.tight(
+                    Size(screenWidth, screenHeight),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return ListView(children: [
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                          "Kode Lapangan Usaha",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                            "A = Pertanian, kehutanan, dan perikanan",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("B = Pertambangan dan penggalian",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("C = Industri",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("D = Pengadaan listrik dan gas",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                            "E = Pengadaan air, pengelolaan sampah/limbah dan daur ulang",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("F = Konstruksi",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                            "G = Perdagangan, reparasi mobil/motor",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("H = Transportasi dan pergudangan",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                            "I = Penyediaan akomodasi dan makan minum",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("J = Informasi dan komunikasi",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("K = Jasa keuangan dan asuransi",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("L = Real estate",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("M_N = Jasa perusahaan",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                            "O = Adm pemerintahan, pertahanan, dam jaminan sosial wajib",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("P = Jasa pendidikan",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                            "Q = Jasa kesehatan dan kegiatan sosial",
+                            textAlign: TextAlign.justify),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        alignment: Alignment.centerLeft,
+                        child: const Text("R_S_T_U = Jasa lainnya",
+                            textAlign: TextAlign.justify),
+                      ),
+                    ],);
+                  },
+                );
+              },
+              icon: const Icon(Icons.info_outlined),
+            ),
+          ],
           leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
